74456ff89d655a1f77d7bd233ee39fe8c5f60147
Griffin Fujioka <fujiokag@hotmail.com>
2017-07-25 01:22:47 -0700

    remove google maps stuff; clean up

--- assets/js/script.js
+++ assets/js/script.js
@@ -176,10 +176,6 @@ var mapProp = {
   disableDefaultUI: true,
   scrollwheel: false
   };
-var map=new google.maps.Map(document.getElementById("googleMap")
-  ,mapProp);
 }
-
-google.maps.event.addDomListener(window, 'load', initialize);
 
 
